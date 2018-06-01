0bcd33270291ad9d21ea56ec3fb8622ff1e1726f
vasy <elsile69@yahoo.com>
2018-06-01 15:56:27 +0300

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -2,7 +2,7 @@
 sudo distrobuilder build-dir ubuntu.yaml rootfs
 sudo tar --numeric-owner --anchored --exclude=./rootfs/dev/log -czf ./rootfs.tar.gz ./rootfs/*
 sudo rm -rf rootfs
-mkdir xenial64
+mkdir box
 cp -v metadata.json lxc-config rootfs.tar.gz box
 tar -czf xenial64.box box/*
 sudo rm -rf box
